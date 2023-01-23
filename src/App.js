3af92a4431d929eb6da680c851ca68aab02acd5c
r-s-i <77388911+r-s-i@users.noreply.github.com>
2023-01-23 20:34:14 +0100

    Compressed and modified the CSS.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <main>
-      <h1>Random Quote Machine</h1>
-      <section id="quote-box">
-        <p id="text">Random quote</p>
-        <p id="author">- Author</p>
-        <button id="new-quote">New quote</button>
-        <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet quote</a>
-      </section>
-    </main>
-    
+      <main>
+        <h1>Random Quote Machine</h1>
+        <section id="quote-box">
+          <p id="text">Random quote</p>
+          <p id="author">- Author</p>
+          <button id="new-quote">New quote</button>
+          <p>
+            <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet quote</a>
+          </p>
+        </section>
+      </main>
 
     </div>
   );
